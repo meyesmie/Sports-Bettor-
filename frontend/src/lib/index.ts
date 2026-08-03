0ec0ef93a287b8edab5712eb37087dda766f7846
meyesmie <meyesmie@gmail.com>
2026-08-03 20:16:21 +0100

    Create index.ts to export library modules

---
+++ frontend/src/lib/index.ts
@@ -0,0 +1,7 @@
+// frontend/src/lib/index.ts
+export { default as axios } from './axios';
+export * from './api';
+export * from './utils';
+export * from './constants';
+export * from './validation';
+export { cn } from './cn';
